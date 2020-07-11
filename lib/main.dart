13755da104b80ea99ca19874ead8646272f29d70
Tsubasa Minagawa <bisuko11@icloud.com>
2020-07-11 15:28:59 +0900

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
                       ),
                     ),
                     RaisedButton(
-                      child: Text('ボタン'),
+                      child: Text('KBOY'),
                       onPressed: () {
                         // ここでなにかする
                         model.changeKboyText();
